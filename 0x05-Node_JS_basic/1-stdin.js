6a7710acd714342c70e02340c79f6aa671cb5c0b
Johnmarare <mararejohn@gmail.com>
2024-12-18 16:04:24 +0000

    standard process input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-const message1 = "Welcome to ALX, what is your name?";
-console.log(message1);
-process.stdin.on("data", data => {
-	    data = data.toString();
-	    process.stdout.write("Your name is: "+ data);
-	    process.exit();
+process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-process.on("exit", data => {
-	console.log("This important software is now closing");
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
